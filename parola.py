c5c032c365b46149ffd20b3a27bc840ce9c0b903
hmmayanayan <ayangahramanli220@gmail.com>
2025-04-18 20:32:13 +0300

    Update parola.py

--- parola.py
+++ parola.py
@@ -1,12 +1,53 @@
+#import random
+
+#data = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+#parola_uzunluğu = int(input("Parola uzunluğunu giriniz"))
+
+#parola = ""
+
+#for i in range(parola_uzunluğu):
+    #parola += random.choice(data)
+
+#print(parola)
+
+import discord
 import random
+from discord.ext import commands
 
-data = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+intents = discord.Intents.default()
+intents.message_content = True
 
-parola_uzunluğu = int(input("Parola uzunluğunu giriniz"))
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-parola = ""
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
 
-for i in range(parola_uzunluğu):
-    parola += random.choice(data)
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
 
-print(parola)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("")
+
